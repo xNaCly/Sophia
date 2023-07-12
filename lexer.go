bfb3caa8fe1839062f389a2325d683a2e2b41a7b
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-07-12 17:11:23 +0200

    feat(lexer): improved error handling
    these errors occur, because floats arent yet implemented
    
        lexer.go:98: err: UNKNOWN token '2' at [l 1:22]
    
        001 |	;; prints hello world to stdout
        002 |	[putv "Hello World!" 25.0]
                                    ^
    
        lexer.go:98: err: UNKNOWN token '5' at [l 1:23]
    
        001 |	;; prints hello world to stdout
        002 |	[putv "Hello World!" 25.0]
                                     ^
    
        lexer.go:98: err: UNKNOWN token '.' at [l 1:24]
    
        001 |	;; prints hello world to stdout
        002 |	[putv "Hello World!" 25.0]
                                      ^
    
        lexer.go:98: err: UNKNOWN token '0' at [l 1:25]
    
        001 |	;; prints hello world to stdout
        002 |	[putv "Hello World!" 25.0]
                                       ^
    
        tisp.go:27: []

--- lexer.go
+++ lexer.go
@@ -4,26 +4,27 @@ import (
 	"errors"
 	"fmt"
 	"log"
-	"os"
 	"strings"
 	"unicode"
 )
 
 type Lexer struct {
-	input   []byte
-	pos     int
-	chr     byte
-	line    int
-	linepos int
+	input    []byte
+	pos      int
+	chr      byte
+	line     int
+	linepos  int
+	hasError bool
 }
 
 func NewLexer(input []byte) Lexer {
 	return Lexer{
-		input:   input,
-		pos:     0,
-		chr:     input[0],
-		line:    0,
-		linepos: 0,
+		input:    input,
+		pos:      0,
+		chr:      input[0],
+		line:     0,
+		linepos:  0,
+		hasError: false,
 	}
 }
 
@@ -58,10 +59,10 @@ func (l *Lexer) Lex() []Token {
 			if unicode.IsLetter(rune(l.chr)) || l.chr == '_' {
 				if t, err := l.ident(); err == nil {
 					token = append(token, t)
-					continue
 				} else {
 					l.error(1, t.Raw)
 				}
+				continue
 			}
 		}
 
@@ -76,25 +77,50 @@ func (l *Lexer) Lex() []Token {
 
 		l.advance()
 	}
+	if l.hasError {
+		return []Token{}
+	}
 	return token
 }
 
 func (l *Lexer) error(errType uint, ident string) {
+	pos := l.linepos
+	iLen := len(ident)
+
 	switch errType {
 	case 2:
-		log.Printf("err: Unterminated String at [%d:%d-%d]", l.line, l.linepos-len(ident), l.linepos)
+		pos = pos - iLen
+		log.Printf("err: Unterminated String at [l %d:%d->%d]", l.line, pos, l.linepos)
 	case 1:
-		log.Printf("err: UNKNOWN identifier '%s' at [%d:%d-%d]", ident, l.line, l.linepos-len(ident), l.linepos)
+		pos = pos - iLen
+		log.Printf("err: UNKNOWN identifier '%s' at [l %d:%d->%d]", ident, l.line, pos, l.linepos)
 	default:
-		log.Printf("err: UNKNOWN token '%c' at [%d:%d]", l.chr, l.line, l.linepos)
+		log.Printf("err: UNKNOWN token '%c' at [l %d:%d]", l.chr, l.line, pos)
 	}
+
 	lines := strings.Split(string(l.input), "\n")
-	if len(lines) > 1 {
-		fmt.Printf("\n%.3d |\t%s\n%.3d |\t%s\n\t%s%c\n\n", l.line, lines[l.line-1], l.line+1, lines[l.line], strings.Repeat(" ", l.linepos), '^')
-	} else {
-		fmt.Printf("\n%.3d |\t%s\n\t%s%c\n\n", l.line+1, lines[l.line], strings.Repeat(" ", l.linepos), '^')
+
+	spaces := pos - 1
+	if iLen > 0 {
+		spaces = pos - iLen
 	}
-	os.Exit(1)
+	if spaces < 0 {
+		spaces = 0
+	}
+	// if string error highlight string start " and predicted end " with ^
+	if errType == 2 {
+		iLen++
+	}
+	// if no identifier given, print one ^
+	if iLen == 0 {
+		iLen += 1
+	}
+	if l.line-1 > -1 {
+		fmt.Printf("\n%.3d |\t%s\n%.3d |\t%s\n\t%s%s\n\n", l.line, lines[l.line-1], l.line+1, lines[l.line], strings.Repeat(" ", spaces), strings.Repeat("^", iLen))
+	} else {
+		fmt.Printf("\n%.3d |\t%s\n\t%s%c\n\n", l.line+1, lines[l.line], strings.Repeat(" ", spaces), '^')
+	}
+	l.hasError = true
 }
 
 func (l *Lexer) string() Token {
