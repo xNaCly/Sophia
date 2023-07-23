b122e0b5aa910e175af6b56ac378154908b14f86
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-07-23 18:23:23 +0200

    refactor: execute sophia scripts without the '-f' flag
    The interpreter now accepts the first argument as the input file,
    contrary to the previous behaviour, that required the input file to be
    specified via the '-f' flag:
    
        sophia -f ./examples/so.phia
    
    Now the streamlined version of the cli:
    
        sophia ./examples/so.phia
    
    The repl can still be accessed when omitting command line arguments, as
    it is common with languages such as python.

--- sophia.go
+++ sophia.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"sophia/core"
@@ -23,29 +21,26 @@ func run(input []byte) ([]float64, error) {
 		return []float64{}, errors.New("parser error")
 	}
 
-	// TODO: temp, until eval is done
-	res, _ := json.MarshalIndent(ast, "", "\t")
-	fmt.Println(string(res))
 	out := core.Eval(ast)
 	return out, nil
 }
 
 func main() {
 	log.SetFlags(log.Ltime)
-	file := flag.String("f", "", "specifiy source file, if not specifiy start repl")
 	execute := flag.String("e", "", "specifiy expression to execute")
 	flag.Parse()
 
 	if len(*execute) != 0 {
 		run([]byte(*execute))
-	} else if len(*file) != 0 {
-		f, err := os.ReadFile(*file)
+	} else if len(os.Args) != 0 {
+		file := os.Args[1]
+		f, err := os.ReadFile(file)
 		if err != nil {
 			log.Fatalf("Failed to open file: %s\n", err)
 		}
 		_, err = run(f)
 		if err != nil {
-			log.Fatalf("error in source file '%s' detected, stopping...", *file)
+			log.Fatalf("error in source file '%s' detected, stopping...", file)
 		}
 	} else {
 		core.Repl(run)
