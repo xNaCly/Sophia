c4067ed664e3fcf4e213bc0f37ef280f64f858b8
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-08-04 15:39:10 +0200

    feat: variable comparison

--- core/expr.go
+++ core/expr.go
@@ -313,11 +313,18 @@ func (e *Equal) GetToken() Token {
 }
 
 func (e *Equal) Eval() any {
-	list := make([]any, len(e.Children))
-	for i, c := range e.Children {
-		list[i] = c.Eval()
+	list := make([]any, 0)
+	for _, c := range e.Children {
+		ev := c.Eval()
+		if val, ok := isType[[]interface{}](ev); ok {
+			list = append(list, val...)
+		} else {
+			list = append(list, ev)
+		}
+	}
+	for i := range list {
 		if i > 0 {
-			if list[i] != list[i-1] {
+			if list[i-1] != list[i] {
 				return false
 			}
 		}
