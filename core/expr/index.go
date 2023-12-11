4ed459ef58ae881fb9c7d6547a17a2a334acc1d3
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-12-11 14:11:15 +0100

    core/expr: improve index error messages
    Indexing via number into string error:
    
        error: Index error
    
                at: /home/teo/programming/Sophia/variables.phia:16:18:
    
                   14|
                   15| ;; (put person.name.test)
                   16| (put person.name.5)
                     |                  ^
    
        Not an array, can't use <target>.5
    
    Indexing into string with identifier:
    
        error: Index error
    
                at: /home/teo/programming/Sophia/variables.phia:15:18:
    
                   13| })
                   14|
                   15| (put person.name.test)
                     |                  ^^^^
                   16| ;; (put person.name.5)
    
        Not an object, can't use <target>.test
    
    Undefined field access:
    
        error: Index error
    
                at: /home/teo/programming/Sophia/variables.phia:15:18:
    
                   13| })
                   14|
                   15| (put person.test.5)
                     |                  ^
    
        Index 5 unavailable on <nil>
    
    Indexing into array with property:
    
        error: Index error
    
                at: /home/teo/programming/Sophia/variables.phia:15:21:
    
                   13| })
                   14|
                   15| (put person.aliases.name)
                     |                     ^^^^
    
        Can't index array.name, not an object
    
    Indexing array out of bounds:
    
        error: Out of bounds error
    
                at: /home/teo/programming/Sophia/variables.phia:15:21:
    
                   13| })
                   14|
                   15| (put person.aliases.3)
                     |                     ^
    
        Array has length of 2, index 3 can not be accessed
    
    Index object with number:
    
        error: Index error
    
                at: /home/teo/programming/Sophia/variables.phia:15:13:
    
                   13| })
                   14|
                   15| (put person.3)
                     |             ^
    
        Can't index object.3, not an array

--- core/expr/index.go
+++ core/expr/index.go
@@ -22,16 +22,27 @@ func (i *Index) GetToken() *token.Token {
 	return i.Token
 }
 
-func indexHelper(parent *Ident, target any, index []Node) any {
+func indexHelper(target any, index []Node) any {
 	switch v := target.(type) {
 	case []interface{}:
 		{
 			// eg: [array.0]
 			in := index[0]
-			idx, ok := in.Eval().(float64)
+			switch V := in.(type) {
+			case *Ident:
+				t := in.GetToken()
+				serror.Add(t, "Index error", "Can't index array.%s, not an object", V.Name)
+				serror.Panic()
+			}
+			idxf, ok := in.Eval().(float64)
 			if !ok {
 				t := in.GetToken()
 				serror.Add(t, "Index error", "Can't index array with %q, use a number", token.TOKEN_NAME_MAP[t.Type])
+				serror.Panic()
+			}
+			idx := int(idxf)
+			if idx > len(v) {
+				serror.Add(in.GetToken(), "Out of bounds error", "Array has length of %d, index %d can not be accessed", len(v), idx)
 				serror.Panic()
 			}
 			curTarget := v[int(idx)]
@@ -41,12 +52,18 @@ func indexHelper(parent *Ident, target any, index []Node) any {
 			}
 
 			// eg: [array.0.x]
-			return indexHelper(parent, curTarget, index[1:])
+			return indexHelper(curTarget, index[1:])
 		}
 	case map[string]interface{}:
 		{
 			// eq: [map.x]
 			in := index[0]
+			switch V := in.(type) {
+			case *Float:
+				t := in.GetToken()
+				serror.Add(t, "Index error", "Can't index object.%g, not an array", V.Value)
+				serror.Panic()
+			}
 			idx, ok := in.(*Ident)
 			if !ok {
 				t := idx.GetToken()
@@ -59,14 +76,23 @@ func indexHelper(parent *Ident, target any, index []Node) any {
 			}
 
 			// eg: [map.x.y]
-			return indexHelper(parent, curTarget, index[1:])
+			return indexHelper(curTarget, index[1:])
 		}
 	case nil:
-		serror.Add(index[0].GetToken(), "Index error", "Can not access nothing (nil)")
+		// TODO: display what part of the index is nil: person.bank.etc
+		//                                                     ^^^^ is null, thus .etc will error
+		val := index[0].Eval()
+		serror.Add(index[0].GetToken(), "Index error", "Index %v unavailable on %v", val, target)
 		serror.Panic()
 	default:
-		serror.Add(index[0].GetToken(), "Index error", "Can not index into %T", target)
-		serror.Panic()
+		switch V := index[0].(type) {
+		case *Ident:
+			serror.Add(index[0].GetToken(), "Index error", "Not an object, can't use <target>.%s", V.Name)
+			serror.Panic()
+		case *Float:
+			serror.Add(index[0].GetToken(), "Index error", "Not an array, can't use <target>.%g", V.Value)
+			serror.Panic()
+		}
 	}
 	return nil
 }
@@ -78,7 +104,7 @@ func (i *Index) Eval() any {
 		serror.Add(ident.Token, "Index error", "Requested element %q not defined", ident.Name)
 		serror.Panic()
 	}
-	return indexHelper(ident, requested, i.Index)
+	return indexHelper(requested, i.Index)
 }
 
 func (i *Index) CompileJs(b *strings.Builder) {
